ca951060f4574701732fbe409dd299c70e14550b
NRMT-MAIN <sahunirmit.main@gmail.com>
2025-03-14 14:56:43 +0530

    Added /Algorithm query

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const FormData = require('form-data');
 const TelegramBot = require('node-telegram-bot-api') ; 
+const fs = require('fs')
+
 
 const token = '8126562032:AAEet4PCFfExxdWFVSRotNbniTWFesn6C6w' ;
 const bot = new TelegramBot(token , {polling : true}) ;
+
+const form = new FormData() ; 
+const dp = fs.createReadStream('./images/DP.webp') ; 
+const bs = fs.createReadStream('./images/binary-search.png') ;
+const sort = fs.createReadStream('./images/Sorting.jpg') ; 
+
+form.append("DP" , dp) ;
+form.append("Binary Search" , bs) ; 
+form.append("Sorting" , sort) ; 
+
+
 
 const getRandomJoke = async () => {
     try {
@@ -19,16 +33,39 @@ const getRandomJoke = async () => {
     }
 }
 
+
+
 bot.on('message' , async (msg) => {
     const chatId = msg.chat.id ;
     const messageText = msg.text ;
+    const message = messageText.split(" ") ; 
 
-    if (messageText === '/start') {
+    if (message[0] === '/start') {
         bot.sendMessage(chatId, 'Welcome to the bot!');
     }
-
-    if(messageText == '/joke') {
+    else if(message[0] == '/joke') {
         const res = await getRandomJoke() ; 
         bot.sendMessage(chatId , res.joke) ; 
     }
+    else if(message[0] == '/algorithm'){
+        let photoPath = "" ;
+        if(message[1] == "DP") photoPath = "./images/DP.webp"
+        else if(message[1] == "Binary-Search") photoPath = "./images/binary-search.png" ;
+        else if(message[1] == "Sorting") photoPath = "./images/Sorting.jpg" ;
+        else{
+            await bot.sendMessage(chatId ,  "Algorithm Available : DP Binary-Search Sorting " ) ;
+            await bot.sendMessage(chatId , "/algorithm <Algorithm Name>")
+            return ; 
+        }
+        try {
+            await bot.sendPhoto(chatId , photoPath) ;
+            console.log("Image sent Succesffull") ; 
+        } catch(err){
+            bot.sendMessage("Some Error Occur")
+        }
+    }
+    else {
+        bot.sendMessage(chatId , "Command Available : /start /algorithm /joke") ; 
+    }
+    
 }) ;
